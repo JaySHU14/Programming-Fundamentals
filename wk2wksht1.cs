a84b76db2cd2f3bfb56b104aae3e4c95fb3d62eb
JaySHU14 <145919670+JaySHU14@users.noreply.github.com>
2023-10-10 13:15:46 +0100

    Add files via upload

---
+++ wk2wksht1.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+// TASK 1
+
+string[] months = new string[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+foreach (string month in months)
+{
+    month = month.toUpper();
+}
+
+
+*/
+
+// TASK 2
+
+string[] tale = new string[]{"it", "was", "the", "winter", "of", "despair."};
+
+for(string i = tale.Length-1; i++)
+{
+    Console.Write(letters[i]);
+}
